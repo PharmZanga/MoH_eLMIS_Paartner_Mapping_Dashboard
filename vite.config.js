3c4e2779fa4a9fdb04b4e1bfc88908060a0ba0c0
PharmZanga <zangamusakuzi@gmail.com>
2026-06-04 20:37:25 +0200

    Add GitHub Pages deployment

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/MoH_eLMIS_Paartner_Mapping_Dashboard/",
+  plugins: [react()]
+});
